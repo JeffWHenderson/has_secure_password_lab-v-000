e71ff3cbdfa8f752a5065e246988f5b4e6b45754
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 21:43:38 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save && @user.authenticate
       session[:user_id] = @user.id
       redirect_to welcome_path
     else
