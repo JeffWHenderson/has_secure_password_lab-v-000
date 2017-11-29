5b1c584db9ba3c271e4bccdcb230dee8906daa97
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 20:48:56 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,3 @@
 class User < ActiveRecord::Base
   has_secure_password
-  #  validates :email, confirmation: true
-  # is valid if password and password_confirmation match
-  # is valid if password is set and password_confirmation is nil
-  # is invalid if password and password_confirmation are both non-nil and don't match
-
-  # returns the user if credentials match
-  # returns falsey if credentials don't match
 end
