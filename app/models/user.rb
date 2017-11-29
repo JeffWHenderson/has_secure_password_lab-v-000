7f9f342f0d125be164a93f298cc339b65d851798
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 20:15:23 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+    validates :email, confirmation: true
+  # is valid if password and password_confirmation match
+  # is valid if password is set and password_confirmation is nil
+  # is invalid if password and password_confirmation are both non-nil and don't match
+  # returns the user if credentials match
+  # returns falsey if credentials don't match
 end
