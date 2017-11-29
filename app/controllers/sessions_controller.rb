9860177e1796ceddcab8f3631afb7356beee1895
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 21:56:08 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to welcome_path
     else
-      redirect_to login_path
+      redirect_to welcome_path
     end
   end
 
