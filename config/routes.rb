4d280cecfe4a7e2a04ae5bf31d35d14587fb4f52
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 21:08:08 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
-  get '/signup' => 'users/new'
-  post '/signup' => 'users/create'
-  #get '/welcome' => 'users/welcome'
-
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#new'
-  post '/logout' => 'sessions#destroy'
+  # get '/signup' => 'users/new'
+  # post '/signup' => 'users/create'
+  # #get '/welcome' => 'users/welcome'
+  #
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#new'
+  # post '/logout' => 'sessions#destroy'
 
 end
