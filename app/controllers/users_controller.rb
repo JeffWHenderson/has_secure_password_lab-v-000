5d7608364d37d77f002489343cfe3fdd9d4b3362
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 22:04:39 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if session.include?(:name)
+      redirect_to welcome_path
+    else
+      @user = User.new
+    end
   end
 
   def create
